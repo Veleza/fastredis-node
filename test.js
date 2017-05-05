189b224be4e048415901a029eeaac44f58007ef3
Vytautas Povilaitis <vytautas@veleza.com>
2017-05-05 17:14:03 +0100

    Updates test

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var fastredis = require('./build/Release/fastredis');
 
-var r = fastredis('138.68.184.121');
+var r = fastredis();
 
 r.select(2);
 
 var start = process.hrtime();
-var data = r.get('users.9a7fb83a-473b-481e-9403-6a9024cdcee4');
+var data = r.get('my_awesome_key');
 var end = process.hrtime(start);
 console.log("Execution time (hr): ", end[0], end[1]/1000000);
 console.log(data);
